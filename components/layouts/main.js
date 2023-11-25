13b722f518b23bd24c68cd9814f0a5a66fedd7c8
xThirteenIII <giuseppe.richetti@gmail.com>
2023-11-25 15:20:54 +0100

    Add three.js dependencies and model section

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import NavBar from "../navbar.js"
 import { Box, Container } from "@chakra-ui/react"
+import VoxelSpace from "../voxel-space.js";
 
 // In Next.js, the Head component is used to modify the HTML <head> tag,
 // and Box is a layout component provided by the Chakra UI library.
@@ -31,6 +32,7 @@ const Main = ({children, router}) => {
             
             <NavBar path={router.asPath} />
             <Container maxW="container.md" pt={16}>
+                <VoxelSpace />
                 {children}
             </Container>
         </Box>
